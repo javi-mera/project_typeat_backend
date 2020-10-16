c9f35b437c58cdb065fc6855dbed172c93e2e106
sarasempere <semperesara@gmail.com>
2020-10-16 13:01:13 +0000

    cambio modelo user

--- src/main.py
+++ src/main.py
@@ -10,17 +10,13 @@ from admin import setup_admin
 from models import db, User, Dish, Restaurant, Gender, Role, SeedData, FileContents, City, SearchDishSearch
 from flask import request
 from flask_sqlalchemy import SQLAlchemy
-
-
 #login
 from werkzeug.security import generate_password_hash, check_password_hash
 import uuid 
 import jwt
 import datetime
 from functools import wraps
-
 app = Flask(__name__)
-
 app.url_map.strict_slashes = False
 app.config['SECRET_KEY']='Th1s1ss3cr3t'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
@@ -29,56 +25,37 @@ MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
-
 #creando carpeta descargas - borrar
 app.config['UPLOAD_FOLDER'] = '/workspace/project_typeat_backend/src/Archivos PDF'
-
 #PROBANDO CORS - borrar   
 app.config['CORS_HEADERS'] = 'Content-Type'
-
-
 # Handle/serialize errors like a JSON object
-
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
-    
     return jsonify(error.to_dict()), error.status_code
-
 # generate sitemap with all your endpoints
-
-
 @app.route('/')
 def sitemap():
     #SeedData.generate_restaurant_and_dishes()
     return generate_sitemap(app)
-
-
 # USERS
-
 #get all users
 @app.route('/user', methods=['GET'])
 def get_users():
     users = User.query.all()
     all_people = list(map(lambda x: x.serialize(), users))
-
     return jsonify(all_people), 200
-
-
-#create user
-@app.route('/user', methods=['POST'])
-def create_user():
-    request_user = request.get_json()
-    user1 = User(email=request_user["email"], 
-    password=request_user["password"], 
-    name=request_user["name"],
-    last_name=request_user["last_name"], 
-    phone=request_user["phone"],
-    gender=Gender[request_user["gender"]].value,
-    role = Role[request_user["role"]].value)
-    db.session.add(user1)
-    db.session.commit()
-
-    return jsonify(user1.serialize()), 200
+#create user, me da errorrrrrrr
+#@app.route('/users', methods=['POST'])
+#def create_user():
+    #request_user = request.get_json()
+    #print(request_user)
+    #user1 = User(
+    #    email=request_user["email"], 
+    #    password=request_user["password"], 
+    #db.session.add(user1)
+    #db.session.commit()
+    #return jsonify(user1.serialize()), 200
 
 # Modificar o traer un user (¿¿Cómo modificar pwd??)
 @app.route('/user/<int:user_id>', methods=['PUT', 'GET'])
@@ -88,22 +65,12 @@ def get_single_user(user_id):
         user1 = User.query.get(user_id)
         if user1 is None:
             raise APIException('User not found', status_code=404)
-        if "name" in body:
-            user1.name = body["name"]
         if "email" in body:
             user1.email = body["email"]
-        if "phone" in body:
-            user1.phone = body["phone"] 
-        if "last_name" in body:
-            user1.last_name = body["last_name"]
-        if "gender" in body:
-            user1.gender = body["gender"]
         db.session.commit()
     if request.method == 'GET':
         user1 = User.query.get(user_id)
-
     return jsonify(user1.serialize()), 200 
-
 #Eliminar un user
 @app.route('/user/<int:user_id>', methods=['DELETE'])
 def delete_single_user(user_id):
@@ -112,20 +79,14 @@ def delete_single_user(user_id):
         raise APIException('User not found', status_code=404)
     db.session.delete(user1)
     db.session.commit()    
-
     return jsonify(user1.serialize()), 200    
-
-
 # DISHES
-
 #get all dishes
 @app.route('/dish', methods=['GET'])
 def get_dishes():
     dishes = Dish.query.all()
     all_dishes = list(map(lambda x: x.serialize(), dishes))
-
     return jsonify(all_dishes), 200
-
 #create dish
 @app.route('/dish', methods=['POST'])
 def create_users():
@@ -137,9 +98,7 @@ def create_users():
         restaurant_id=request_dish["restaurant_id"]) #no sé si hay que ponerlo o se puede establecer la relación desde models.
     db.session.add(dish1)
     db.session.commit()
-
     return jsonify(dish1.serialize()), 200
-
 #probando a cargar archivos
 @app.route('/upload', methods=['GET','PUT'])
 def upload():
@@ -147,10 +106,7 @@ def upload():
         file = request.files["fileinput"]
         file.save(os.path.join(app.config['UPLOAD_FOLDER'],file.filename ))
         #print("algop")
-        
         return redirect(request.url)
-
-  
 # Modificar o traer un dish
 @app.route('/dish/<int:dish_id>', methods=['PUT', 'GET'])
 def get_single_dish(dish_id):
@@ -166,9 +122,7 @@ def get_single_dish(dish_id):
         db.session.commit()
     if request.method == 'GET':
         dish1 = Dish.query.get(dish_id)
-
     return jsonify(dish1.serialize()), 200 
-
 #Eliminar un dish
 @app.route('/dish/<int:dish_id>', methods=['DELETE'])
 def delete_single_dish(dish_id):
@@ -177,19 +131,13 @@ def delete_single_dish(dish_id):
         raise APIException('Dish not found', status_code=404)
     db.session.delete(dish1)
     db.session.commit()    
-
     return jsonify(dish1.serialize()), 200  
-
-
 # RESTAURANT
-
 # get all restaurant
 @app.route('/restaurant', methods=['GET'])
 def get_restaurant():
-
     restaurant = Restaurant.query.all()
     all_restaurants = list(map(lambda x: x.serialize(), restaurant))
-
     return jsonify(all_restaurants), 200
 
 # create restaurant
@@ -208,9 +156,7 @@ def create_restaurant():
     city_id=request_restaurant["city_id"]  ) #no sé si hay que ponerlo o se puede establecer la relación desde models.
     db.session.add(restaurant1)
     db.session.commit()
-
     return jsonify(restaurant1.serialize()), 200
-
 # Modificar o traer un restaurant
 @app.route('/restaurant/<int:restaurant_id>', methods=['PUT', 'GET'])
 def get_single_restaurant(restaurant_id):
@@ -232,9 +178,7 @@ def get_single_restaurant(restaurant_id):
         db.session.commit()
     if request.method == 'GET':
         restaurant1 = Restaurant.query.get(restaurant_id)
-
     return jsonify(restaurant1.serialize()), 200 
-
 #Eliminar un restaurant
 @app.route('/restaurant/<int:restaurant_id>', methods=['DELETE'])
 def delete_single_restaurant(restaurant_id):
@@ -243,18 +187,13 @@ def delete_single_restaurant(restaurant_id):
         raise APIException('Restaurant not found', status_code=404)
     db.session.delete(restaurant1)
     db.session.commit()    
-
     return jsonify(restaurant1.serialize()), 200    
-
 # get all cities
 @app.route('/city', methods=['GET'])
 def get_cities():
-
     cities = City.query.all()
     all_cities = list(map(lambda x: x.serialize(), cities))
-
     return jsonify(all_cities), 200
-
 # create city
 @app.route('/city', methods=['GET'])
 def create_city():
@@ -266,9 +205,7 @@ def create_city():
     )
     db.session.add(city1)
     db.session.commit()
-
     return jsonify(city1.serialize()), 200
-
 # Modificar o traer una city
 @app.route('/city/<int:city_id>', methods=['PUT', 'GET'])
 def get_single_city(city_id):
@@ -282,31 +219,24 @@ def get_single_city(city_id):
         db.session.commit()
     if request.method == 'GET':
         city1 = City.query.get(city_id)
-
     return jsonify(city1.serialize()), 200 
-
-
 @app.route('/search', methods=['GET', 'POST'])
 def search_results():
     args2=request.args.to_dict(flat=False)
     lugar =''
     plato =''
-
     if args2['lugar'] != ['']:
         lugar = args2['lugar'][0].lower()
     else:
        return jsonify({'msg':'Error'}), 301
         #lugar = "vacío"
-
     if args2['plato'] != ['undefined']:
         plato = args2['plato'][0].lower()
     else:
         plato = args2['plato']
     #print(lugar,1)
     #print(plato,2)
-    
     return "Not query string", 200  
-
 @app.route('/render_results', methods=['GET'])
 def render_results():
     args2=request.args.to_dict(flat=False)
@@ -316,16 +246,12 @@ def render_results():
     elif not('lugar'in args2):
       raise APIException('Dish not found', status_code=400)
     else:
-        
       city = args2['lugar'][0].lower() if 'lugar' in args2 else None
       dish = args2['plato'][0].lower() if 'plato' in args2 and len(args2['plato'][0])>0 else None 
       seeker = SearchDishSearch()
       dishes = seeker.search(city, dish)
       #print(dishes)
     return jsonify({"info": dishes}), 200  
-  
-
-
 @app.route('/restaurantInfo/<int:rest_id>', methods=['GET'])
 def restInfo(rest_id):
     body = rest_id
@@ -334,60 +260,42 @@ def restInfo(rest_id):
     rest = Restaurant.query.get(rest_id)
     print(rest)
     return jsonify(rest.serialize()), 200   
-
-
-
 def token_required(f):  
     @wraps(f)  
     def decorator(*args, **kwargs):
-
         token = None 
-
         if 'x-access-tokens' in request.headers:  
             token = request.headers['x-access-tokens'] 
-
-
         if not token:  
             return jsonify({'message': 'a valid token is missing'})   
-
-
         try:  
             data = jwt.decode(token, app.config["SECRET_KEY"]) 
             current_user = Users.query.filter_by(id=data['id']).first()  
         except:  
             return jsonify({'message': 'token is invalid'})
-        
         return f(current_user, *args, **kwargs)
-
     return decorator 
-
-
 @app.route('/register', methods=['GET', 'POST'])
 def signup_user():  
  data = request.get_json()  
-
- hashed_password = generate_password_hash(data['password'], method='sha256')
- 
- new_user = User(name=data['name'], last_name=data['last_name'], phone=data['phone'], email=data['email'], gender = Gender[data['gender']].value, password=hashed_password, role = Role[data['role']].value) 
+ print(data)
+ #hashed_password = generate_password_hash(data['password'], method='sha256')
+ new_user = User(email=data['email'], password=data['password']) 
+ print(new_user)
  db.session.add(new_user)  
  db.session.commit()    
-
- return jsonify({'message': 'registered successfully'})   
-
+ return jsonify({'message': 'registered successfully'})  
 
 @app.route('/login', methods=['GET', 'POST'])  
 def login_user(): 
- 
     auth = request.authorization  
     print(auth)
-     
-    if not auth or not auth.username or not auth.password:  
+    if not auth or not auth["username"] or not auth["password"]:  
         return make_response('could not verify', 401, {'WWW.Authentication': 'Basic realm: "login required"'})    
-
-    user = User.query.filter_by(email=auth.username).first()   
-   
-    if check_password_hash(user.password, auth.password):
-        token = jwt.encode({'id': user.id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])  
+    user = User.query.filter_by(email=auth["username"]).first()   
+    print(user.password, auth["password"])
+    if user.password == auth["password"]:
+        token = jwt.encode({'id': user.id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY']) 
+        print("4",token) 
         return jsonify({'token' : token.decode('UTF-8')}) 
-        print(token)    
     return make_response('could not verify',  401, {'WWW.Authentication': 'Basic realm: "login required"'})
--- src/models.py
+++ src/models.py
@@ -1,63 +1,46 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 from flask_sqlalchemy import SQLAlchemy;
 from enum import Enum;
 import json
-
 db = SQLAlchemy()
-
 class Gender(Enum):
   male = 1
   female = 2
   other = 3
-
 class Role(Enum):#cómo funciona?
   foodie = 1
   manager = 2
-
 class User(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
-    role= db.Column(db.Integer(), unique=False, nullable=False)
-    name = db.Column(db.String(120), unique=False, nullable=False)
-    last_name = db.Column(db.String(120), unique=False, nullable=False)
-    phone = db.Column(db.Integer(), unique=True, nullable=True)
+    #role= db.Column(db.Integer(), unique=False, nullable=False)
+    #name = db.Column(db.String(120), unique=False, nullable=False)
+    #last_name = db.Column(db.String(120), unique=False, nullable=False)
+    #phone = db.Column(db.Integer(), unique=True, nullable=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    gender = db.Column(db.Integer(), unique=False, nullable=False)#hace falta poner nº de caracteres?
+    #gender = db.Column(db.Integer(), unique=False, nullable=False)#hace falta poner nº de caracteres?
     is_active = db.Column(db.Boolean(), unique=False, nullable=False, default=True)#preguntar si puede dejarse como nullable True, ya que al crear usuario no se solicita el dato.
     
     
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-
     def serialize(self):
         return {
             "id": self.id,
-            "name":self.name,
-            "last_name": self.last_name,
-            "phone":self.phone,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
-
 preferredDishes = db.Table("preferredDishes",
     db.Column("dish_id", db.Integer, db.ForeignKey("dish.id"), primary_key=True),
     db.Column("user_id", db.Integer, db.ForeignKey("user.id"), primary_key=True),
-
 )
-
 class City(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(120), unique=False, nullable=False)
     #latitude = db.Column(db.String(120), unique=True, nullable=False)
     #longitude = db.Column(db.String(120), unique=True, nullable=False)
     restaurants = db.relationship("Restaurant", lazy=True)
-
     def __repr__(self):
         return '<City %r>' % self.name
-
     def serialize(self):
         return {
             "id": self.id,
@@ -65,7 +48,6 @@ class City(db.Model):
         
         }
     
-
 class Restaurant(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(120), unique=False, nullable=False)
@@ -83,10 +65,8 @@ class Restaurant(db.Model):
     dishes = db.relationship("Dish", lazy=True)
  
     
-
     def __repr__(self):
         return '<Restaurant %r>' % self.name
-
     def serialize(self):
         return {
             "id": self.id,
@@ -100,8 +80,6 @@ class Restaurant(db.Model):
             "longitude": self.longitude
             # do not serialize the password, its a security breach
         }
-
-
 class Dish(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=False, nullable=False)
@@ -110,10 +88,8 @@ class Dish(db.Model):
     restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurant.id"), nullable = True)
     restaurant = db.relationship("Restaurant", lazy=True)
     img = db.Column(db.String(520), unique=False, nullable=False)
-
     def __repr__(self):
         return '<Dish %r>' % self.name
-
     def serialize(self):
         return {
             "id": self.id,
@@ -125,22 +101,16 @@ class Dish(db.Model):
           
             # do not serialize the password, its a security breach
         }
-
 class FileContents(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(120))
     data = db.Column(db.LargeBinary)
-
 class SeedData():
-
     @staticmethod
     def generate_restaurant_and_dishes():
-
         #with open("./Data/cities.json") as f:
          #   data = json.load(f)
           #  print(data)
-
-
         dishes= [{
             "description": "Bacon ipsum dolor amet cupim jerky ribeye picanha kevin biltong shoulder pork belly tri-tip.",
             "is_typical": True, 
@@ -186,12 +156,10 @@ class SeedData():
         ) 
         db.session.add(restaurant1)
         db.session.commit()
-
         for dish in dishes:
             dish1 = Dish(name=dish["name"], is_typical=dish["is_typical"], description=dish["description"], restaurant_id= restaurant1.id)
             db.session.add(dish1)
             db.session.commit() 
-
 class SearchDishSearch:
   def search(self,city,plate):
     results = []
@@ -199,13 +167,11 @@ class SearchDishSearch:
     #print(plate)
     if city is not None and plate is not None:
       results = self.search_by_city_and_plate(city,plate)
-
     elif city is not None:
       #print(city, "aquí")  
       results = self.search_by_city(city)
       
     return results
-
   def search_by_city(self,city):
     cities = City.query.filter_by(name=city).first()
     restaurant_ids = db.session.query(Restaurant.id).filter(Restaurant.city_id == cities.id).all()
@@ -216,7 +182,6 @@ class SearchDishSearch:
     results_r = list(map(lambda x: x.serialize(), results))
     #print(results_r)
     return results_r
-
   def search_by_city_and_plate(self,city,plate):
     #print(plate)
     cities = City.query.filter_by(name=city).first()
@@ -234,4 +199,3 @@ class SearchDishSearch:
     city_dishes_m = list(map(lambda x: x.serialize(), city_dishes))
     print(city_dishes_m)
     return city_dishes_m
-
